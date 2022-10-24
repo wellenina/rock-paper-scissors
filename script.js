6ef06e570664334056e816c86f154061728818f8
wellenina <giulia.fanzaghi@gmail.com>
2022-10-24 15:15:32 +0200

    Add playRound() function

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ function getComputerChoice() {
     const options = ["Rock", "Paper", "Scissors"];
     return options[Math.floor(Math.random()*3)];
 }
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+
+function playRound(playerSelection, computerSelection) {
+
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
+
+    const pairs = {
+        "Rock": "Scissors",
+        "Paper": "Rock",
+        "Scissors": "Paper"
+    }
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (pairs[playerSelection] === computerSelection) {
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`;
+    }
+
+}
