a25b14a2d834401cae23a9ee902f256a2d7f3fd6
wellenina <giulia.fanzaghi@gmail.com>
2022-10-24 16:02:50 +0200

    Add game() function

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ function getComputerChoice() {
     return options[Math.floor(Math.random()*3)];
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-
+let playerSelection = "";
+let computerSelection = "";
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
-
-    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
 
     const pairs = {
         "Rock": "Scissors",
@@ -20,9 +19,24 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "It's a tie!";
     } else if (pairs[playerSelection] === computerSelection) {
+        playerScore += 1;
         return `You win! ${playerSelection} beats ${computerSelection}!`;
     } else {
+        computerScore += 1;
         return `You lose! ${computerSelection} beats ${playerSelection}!`;
     }
 
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt("Choose you weapon!");
+        playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+        computerSelection = getComputerChoice();
+        console.log(`The computer chose ${computerSelection}`)
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Your score: ${playerScore} - Computer score: ${computerScore}`)
+    }
+
+    return playerScore === computerScore ? "It's a tie!" : playerScore > computerScore ? "You win the game!" : "You lose the game!";
 }
