fd25ee113e6642f969e84f412e55f7c1cbe29ce0
wellenina <giulia.fanzaghi@gmail.com>
2022-11-09 16:03:38 +0100

    Fix script.js - First working version

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let playerChoice = "";
 let computerChoice = "";
+let playerChoice = "";
 
+const optionsSection = document.querySelector('.choose');
 const options = document.querySelectorAll('.options li');
-options.forEach(option => option.addEventListener('click', playRound)); // la funzione che fa partire il gioco
+options.forEach(option => option.addEventListener('click', playRound));
 
 const computerScoreDisplay = document.getElementById('computer-score');
 let computerScore = 0;
@@ -28,98 +29,97 @@ function startCountdown() {
 }
 
 function getComputerChoice() {
-    return whatBeatsWhat[Math.floor(Math.random()*10)][0]; // al momento non funziona perché l'array non è in scope
+    const options = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+    return options[Math.floor(Math.random()*5)];
 }
 
-const fistIcons = document.querySelectorAll('.fist-icon'); // closed fist icons
+const fistIcons = document.querySelectorAll('.fist-icon');
 const computerChoiceIcon = document.querySelector('.computer-choice-icon');
 const playerChoiceIcon = document.querySelector('.player-choice-icon');
 
 function assignChoiceIcons() {
-  computerChoiceIcon.innerHTML = '&#x1F596;'; // EXAMPLE: lizard
-  // = computerChoice; // DA CORREGGERE! deve essere il codice dell'icona, non il 'nome' della scelta
-  playerChoiceIcon.innerHTML = '&#x1F90F;'; // EXAMPLE: spock
-  // = playerChoice; // DA CORREGGERE! deve essere il codice dell'icona, non il 'nome' della scelta
+    const icons = {
+        rock: '&#x270A;',
+        paper: '&#x270B;',
+        scissors: '&#x270C;',
+        lizard: '&#x1F90F;',
+        spock: '&#x1F596;'
+        };
+    
+    computerChoiceIcon.innerHTML = icons[computerChoice];
+    playerChoiceIcon.innerHTML = icons[playerChoice];
 }
 
 function revealChoices() {
-  fistIcons.forEach(icon => icon.classList.toggle("transparent"));
+    fistIcons.forEach(icon => icon.classList.toggle("transparent"));
 }
 
-
-
-const resultOverlay = document.getElementById('result-overlay'); // da mettere in display block
+const resultOverlay = document.getElementById('result-overlay');
 resultOverlay.addEventListener('click', function() {
-    // funzione per spegnere l'overlay
-    // & resetta la situazione: (controllare che si riferiscano agli elemetni giusti)
-    options.forEach(option => option.classList.remove('chosen'); // il tasto illuminato torna normale (come selezionare il singolo tasto?)
-    options.forEach(option => option.addEventListener('click', playRound)); // i tasti tornano cliccabili
-    options.forEach(option => option.classList.toggle('disabled')); // i tasti riprendono effetto di hover
-    revealHands(); // le mani si rigirano verso il pugno chiuso (aggiungere un parametro alla funzione?)
+    resultOverlay.style.display = "none";
+    options.forEach(option => option.firstElementChild.classList.remove('chosen'));
+    options.forEach(option => option.addEventListener('click', playRound));
+    optionsSection.classList.toggle('disabled');
+    revealChoices();
 });
-const resultContent = document.getElementById('result-content');
 
+const resultContentDisplay = document.getElementById('result-content');
+let resultContent = '';
 
-
-
-
-// DA SISTEMARE //
-function finishGame(playerChoice, computerChoice) { // gli argomenti devono essere stringe con il nome della scelta!
-
-    /* 
-    compare messaggio in overlay tipo: 'Spock vaporizes Rock! You win!' e si incrementano i punteggi
-    dunque:
-    stabilisce chi ha vinto, incrementa il punteggio e sceglie il messaggio (tipo 'Spock vaporizes Rock! You win!')
-    aggiorna a schermo il punteggio: computerScore / playerScore .textContent = ...
-    assegna il messaggio al div per l'overlay: resultContent.textContent = ''
-    cambia il display del messaggio in overlay tipo = 'block'
-    */
+function finishGame(playerChoice, computerChoice) {
 
     if (playerChoice === computerChoice) {
-        return "Wow! It's a tie!"; // se è PAREGGIO, esce subito dalla funzione
+        resultContent = "Wow! It's a tie!";
+        return;
     }
 
     const whatBeatsWhat = [
-        ['Scissors', 'Paper', 'Scissors cuts Paper'],
-        ['Paper', 'Rock', 'Paper covers Rock'],
-        ['Rock', 'Lizard', 'Rock crushes Lizard'],
-        ['Lizard', 'Spock', 'Lizard poisons Spock'],
-        ['Spock', 'Scissors', 'Spock smashes Scissors'],
-        ['Scissors', 'Lizard', 'Scissors decapitates Lizard'],
-        ['Lizard', 'Paper', 'Lizard eats Paper'],
-        ['Paper', 'Spock', 'Paper disproves Spock'],
-        ['Spock', 'Rock', 'Spock vaporizes Rock'],
-        ['Rock', 'Scissors', 'Rock crushes Scissors (as it always has)']
+        ['scissors', 'paper', 'Scissors cuts Paper'],
+        ['paper', 'rock', 'Paper covers Rock'],
+        ['rock', 'lizard', 'Rock crushes Lizard'],
+        ['lizard', 'spock', 'Lizard poisons Spock'],
+        ['spock', 'scissors', 'Spock smashes Scissors'],
+        ['scissors', 'lizard', 'Scissors decapitates Lizard'],
+        ['lizard', 'paper', 'Lizard eats Paper'],
+        ['paper', 'spock', 'Paper disproves Spock'],
+        ['spock', 'rock', 'Spock vaporizes Rock'],
+        ['rock', 'scissors', 'Rock crushes Scissors (as it always has)']
     ]
 
     for (let i = 0; i < whatBeatsWhat.length; i++) {
         if (playerChoice === whatBeatsWhat[i][0] && computerChoice === whatBeatsWhat[i][1]) {
-            playerScore += 1;
-            return `${whatBeatsWhat[i][2]}! You win!`; // se il player ha vinto, qua esce dalla funzione
+            playerScoreDisplay.textContent = ++playerScore;
+            resultContent = `${whatBeatsWhat[i][2]}! You win!`;
+            return;
         } else if (computerChoice === whatBeatsWhat[i][0] && playerChoice === whatBeatsWhat[i][1]) {
-            computerScore += 1;
-            return `${whatBeatsWhat[i][2]}! You lose!`; // se ha perso, esce qua
+            computerScoreDisplay.textContent = ++computerScore;
+            resultContent = `${whatBeatsWhat[i][2]}! You lose!`;
+            return;
         }
     }
 }
 
+function pause(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
+async function playRound() {
 
-function playRound() { // funzione chiamata dal click del giocatore
+    playerChoice = this.getAttribute('id');
+    this.firstElementChild.classList.toggle('chosen');
+    options.forEach(option => option.removeEventListener('click', playRound));
+    optionsSection.classList.toggle('disabled');
 
-    playerChoice = this.getAttribute('id'); // stringa con il nome dell'opzione scelta (?) DOVE MI SERVE QUESTO DATO??
-    this.classList.toggle('chosen'); // il tasto scelto si illumina - siamo sicuri che illumini l'elemento giusto?
-    options.forEach(option => option.removeEventListener('click', playRound)); // i tasti non sono più cliccabili
-    options.forEach(option => option.classList.toggle('disabled')); // i tasti non hanno effetto di hover - controllare che sia el. giusto
+    setTimeout(startCountdown(), 1000);
 
-    startCountdown(); // parte COUNTDOWN in OVERLAY
+    await pause(4000);
+    computerChoice = getComputerChoice();
+    assignChoiceIcons();
+    revealChoices();
 
-    computerChoice = getComputerChoice(); // genera scelta del computer
+    await pause(2000);
+    finishGame(playerChoice, computerChoice);
 
-    assignChoiceIcons() // assegno le "mani" al computer e al giocatore
-
-    revealChoices() // parte animazione che rivela le mani
-
-    // aspetta circa 2 secondi poi chiama
-    finishGame();
-} 
+    resultContentDisplay.textContent = resultContent;
+    resultOverlay.style.display = 'block';
+}
